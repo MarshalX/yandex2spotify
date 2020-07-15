91dddc2bda88f5cad18b4da792522f08758d20fb
Dmitry Grigoryev <dmitry@dmitryg.name>
2020-07-15 17:59:44 +0300

    Rework search algorithm

--- main.py
+++ main.py
@@ -22,39 +22,35 @@ spotify_client = spotipy.Spotify(auth_manager=auth_manager)
 yandex_playlists = yandex_client.users_playlists_list()
 user = spotify_client.me()['id']
 
-
 def main():
     print(f'User: {user}')
-    for yandex_playlist in [yandex_playlists[2]]:
+    for yandex_playlist in [yandex_playlists[1]]:
         playlist_title = yandex_playlist.title
         spotify_playlist = spotify_client.user_playlist_create(user, playlist_title)
         spotify_playlist_id = spotify_playlist['id']
-        print(f'Import playlist {playlist_title}')
+        print(f'Importing playlist {playlist_title}')
         yandex_tracks = yandex_playlist.fetch_tracks()
         spotify_tracks = []
         for yandex_track in yandex_tracks:
-            track_available = yandex_track['track']['available']
-            if track_available:
-                track_artist = yandex_track['track']['artists'][0]['name']
-                track_title = yandex_track['track']['title']
-                print(f'Finding track: {track_artist} - {track_title}...', end='')
-                found_tracks = spotify_client.search(f'{track_artist} - {track_title}',
-                                                     type='track')['tracks']['items']
-                filter_tracks = [track for track in found_tracks if track['name'].lower().startswith(track_title.lower())]
-                if len(filter_tracks) > 0:
-                    spotify_tracks.append(filter_tracks[0]['id'])
-                    print('\tOK')
-                else:
-                    print('\tNO')
-                if len(spotify_tracks) == 100:
-                    print(f'Adding {len(spotify_tracks)} in playlist {playlist_title}...', end='')
-                    spotify_client.user_playlist_add_tracks(user, spotify_playlist_id, spotify_tracks)
-                    spotify_tracks.clear()
-                    print('\tOK')
+            if not yandex_track['track']['available']:
+                continue
+            track_artist = yandex_track['track']['artists'][0]['name']
+            track_album = yandex_track['track']['albums'][0]['title']
+            track_title = yandex_track['track']['title']
+            print(f'Searching for track: {track_artist} - {track_title}...', end='')
+            found_tracks = spotify_client.search(f'{track_artist} {track_title}',
+                                                    type='track')['tracks']['items']
+            if len(found_tracks) > 0:
+                spotify_tracks.append(found_tracks[0]['id'])
+                print('\tFOUND')
+            else:
+                print('\tFAILED')
         if len(spotify_tracks) > 0:
-            print(f'Adding {len(spotify_tracks)} in playlist {playlist_title}...', end='')
+            print(f'Adding {len(spotify_tracks)} out of {len(yandex_tracks)} tracks to playlist {playlist_title}...', end='')
             spotify_client.user_playlist_add_tracks(user, spotify_playlist_id, spotify_tracks)
             print('\tOK')
+        else:
+            print('NO TRACKS FOUND')
 
 
 if __name__ == '__main__':
