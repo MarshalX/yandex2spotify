21b64e22f26b3578531d5a67736a52555a2699c5
lemonpaul <leamonpaul@yandex.ru>
2020-07-15 20:40:09 +0300

    Don't filter results by track name

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ yandex_client = Client.from_token(TOKEN)
 auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
                             client_secret=CLIENT_SECRET,
                             redirect_uri=REDIRECT_URI,
-                            scope='playlist-modify-public, user-library-modify', username=USERNAME)
+                            scope='playlist-modify-public, user-library-modify, user-library-read', username=USERNAME)
 spotify_client = spotipy.Spotify(auth_manager=auth_manager)
 yandex_playlists = yandex_client.users_playlists_list()
 user = spotify_client.me()['id']
@@ -27,21 +27,22 @@ def main():
     print(f'User: {user}')
     likes_tracks = yandex_client.users_likes_tracks().tracks
     spotify_tracks = []
-    print(f'Saving liked tracks...')
+    unimport_tracks = []
+    print(f'Importing liked tracks...')
     for track in likes_tracks:
         yandex_track = track.fetch_track()
         track_available = yandex_track['available']
         if track_available:
             track_artist = yandex_track['artists'][0]['name']
             track_title = yandex_track['title']
-            print(f'Finding track: {track_artist} - {track_title}...', end='')
+            print(f'Importing track: {track_artist} - {track_title}...', end='')
             found_tracks = spotify_client.search(f'{track_artist} - {track_title}',
                                                  type='track')['tracks']['items']
-            filter_tracks = [track for track in found_tracks if track['name'].casefold().startswith(track_title.casefold())]
-            if len(filter_tracks) > 0:
-                spotify_tracks.append(filter_tracks[0]['id'])
+            if len(found_tracks) > 0:
+                spotify_tracks.append(found_tracks[0]['id'])
                 print('\tOK')
             else:
+                unimport_tracks.append(f'{track_artist} - {track_title}')
                 print('\tNO')
             if len(spotify_tracks) == 50:
                 print(f'Saving {len(spotify_tracks)} tracks...', end='')
@@ -67,11 +68,11 @@ def main():
                 print(f'Finding track: {track_artist} - {track_title}...', end='')
                 found_tracks = spotify_client.search(f'{track_artist} - {track_title}',
                                                      type='track')['tracks']['items']
-                filter_tracks = [track for track in found_tracks if track['name'].casefold().startswith(track_title.casefold())]
-                if len(filter_tracks) > 0:
-                    spotify_tracks.append(filter_tracks[0]['id'])
+                if len(found_tracks) > 0:
+                    spotify_tracks.append(found_tracks[0]['id'])
                     print('\tOK')
                 else:
+                    unimport_tracks.append(f'{track_artist} - {track_title}')
                     print('\tNO')
                 if len(spotify_tracks) == 50:
                     print(f'Adding {len(spotify_tracks)} tracks in playlist {playlist_title}...', end='')
@@ -82,6 +83,10 @@ def main():
             print(f'Adding {len(spotify_tracks)} tracks in playlist {playlist_title}...', end='')
             spotify_client.user_playlist_add_tracks(user, spotify_playlist_id, spotify_tracks)
             print('\tOK')
+        if len(unimport_tracks) > 0:
+            print('Error importing tracks:')
+            for track in unimport_tracks:
+                print(track)
 
 
 if __name__ == '__main__':
