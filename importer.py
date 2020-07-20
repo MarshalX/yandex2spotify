91293c63a216012e18237bafb20d6fe884899a7c
lemonpaul <leamonpaul@yandex.ru>
2020-07-20 14:08:12 +0300

    Fetch each track for collective playlists

--- importer.py
+++ importer.py
@@ -146,7 +146,10 @@ class Importer:
             self.not_imported[playlist.title] = []
 
             playlist_tracks = playlist.fetch_tracks()
-            tracks = [track.track for track in playlist_tracks]
+            if playlist.collective:
+                tracks = [track.fetch_track() for track in playlist_tracks]
+            else:
+                tracks = [track.track for track in playlist_tracks]
 
             def save_tracks_callback(importer, spotify_tracks):
                 logger.info(f'Saving {len(spotify_tracks)} tracks in playlist {playlist.title}...')
