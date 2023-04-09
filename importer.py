5b05ac15dfd45f66f8366e0717d88e1e9d99cba4
k1mst0n <ik1mst0n@gmail.com>
2023-04-09 21:00:06 +0300

    Remove explicit cache_path parameter for Spotify client

--- importer.py
+++ importer.py
@@ -242,8 +242,7 @@ if __name__ == '__main__':
         client_secret=arguments.secret,
         redirect_uri=REDIRECT_URI,
         scope='playlist-modify-public, user-library-modify, user-follow-modify, ugc-image-upload',
-        username=arguments.spotify,
-        cache_path='cache.txt'
+        username=arguments.spotify
     )
     spotify_client_ = spotipy.Spotify(auth_manager=auth_manager, requests_timeout=arguments.timeout)
 
